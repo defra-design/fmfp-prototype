08ba1af2659b70c4fd077baddcce6bca42a4318c
Robin Parker <robinkristianparker@gmail.com>
2019-12-05 14:40:14 +0000

    Added dynamic map to 'Refine your location' page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,4 +7,42 @@ if (window.console && window.console.info) {
 
 $(document).ready(function () {
   window.GOVUKFrontend.initAll()
+
+
+  var baseMapLayer = new ol.layer.Tile({
+    source: new ol.source.OSM()
+  });
+  var map = new ol.Map({
+    target: 'map',
+    layers: [ baseMapLayer],
+    view: new ol.View({
+            center: ol.proj.fromLonLat([0.90401, 51.80645]),
+            zoom: 16 //Initial Zoom Level
+          })
+  });
+  //Adding a marker on the map
+  var marker = new ol.Feature({
+    geometry: new ol.geom.Point(
+      ol.proj.fromLonLat([0.90401, 51.80645]) // postcode CO5 7QG
+    ),
+  });
+
+  marker.setStyle(new ol.style.Style({
+          image: new ol.style.Icon(({
+              crossOrigin: 'anonymous',
+              src: '/public/images/pin.png'
+          }))
+      }));
+
+  var vectorSource = new ol.source.Vector({
+    features: [marker]
+  });
+  var markerVectorLayer = new ol.layer.Vector({
+    source: vectorSource,
+  });
+  map.addLayer(markerVectorLayer);
+
+
+
+
 })

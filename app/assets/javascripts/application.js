0c34bb0bc0773790e85fec37255e6450a17b3142
Robin Parker <robinkristianparker@gmail.com>
2019-12-16 15:35:45 +0000

    Added 'Draw shape' feature to prototype map

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,17 +9,25 @@ $(document).ready(function () {
   window.GOVUKFrontend.initAll()
 
 
+
+  var source = new ol.source.Vector({wrapX: false});
+  var vector = new ol.layer.Vector({
+    source: source
+  });
+
   var baseMapLayer = new ol.layer.Tile({
     source: new ol.source.OSM()
   });
   var map = new ol.Map({
     target: 'map',
-    layers: [ baseMapLayer],
+    layers: [ baseMapLayer, vector],
     view: new ol.View({
             center: ol.proj.fromLonLat([0.90401, 51.80645]),
-            zoom: 16 //Initial Zoom Level
+            zoom: 15 //Initial Zoom Level
           })
   });
+
+
   //Adding a marker on the map
   var marker = new ol.Feature({
     geometry: new ol.geom.Point(
@@ -43,6 +51,69 @@ $(document).ready(function () {
   map.addLayer(markerVectorLayer);
 
 
+  var typeSelect = document.getElementById('type');
+
+  var draw; // global so we can remove it later
+  // function addInteraction() {
+  //   var value = typeSelect.value;
+  //   if (value !== 'None') {
+  //     draw = new ol.interaction.Draw({
+  //       source: source,
+  //       type: 'Polygon'
+  //     });
+  //     map.addInteraction(draw);
+  //   }
+  // }
+
+  function addInteraction() {
+
+      draw = new ol.interaction.Draw({
+        source: source,
+        type: 'Polygon'
+      });
+      map.addInteraction(draw);
+
+  }
+
+
+  var drawShapeBtn = document.getElementById('drawShapeBtn');
+  var markerBtn = document.getElementById('markerBtn');
+
+  drawShapeBtn.addEventListener('click', function (event) {
+    if ( this.classList.contains('active') ) {
+      this.classList.remove('active')
+      map.removeInteraction(draw);
+      vector.setVisible(false);
+      markerVectorLayer.setVisible(true);
+    } else {
+      this.className += " active";
+      map.removeInteraction(draw);
+      addInteraction();
+      vector.setVisible(true);
+      markerVectorLayer.setVisible(false);
+    }
+  }, false);
+
+  markerBtn.addEventListener('click', function (event) {
+    if ( this.classList.contains('active') ) {
+      this.classList.remove('active')
+      markerVectorLayer.setVisible(false);
+    } else {
+      this.className += " active";
+      markerVectorLayer.setVisible(true);
+      map.removeInteraction(draw);
+    }
+  }, false);
+
+
+
+
+
+
+
+
+
+  
 
 
 })

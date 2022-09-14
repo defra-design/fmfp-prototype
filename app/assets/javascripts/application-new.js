b2e273197bf74ef0002b1835ad1182d2e4bc11d6
Mark Fee <Mark.Fee@defra.gov.uk>
2022-09-14 12:13:11 +0100

    applied standard.js to application-new.js

--- app/assets/javascripts/application-new.js
+++ app/assets/javascripts/application-new.js
@@ -1,4 +1,4 @@
-/* global $ */
+/* global $, ol */
 
 // Warn about using the kit in production
 if (window.console && window.console.info) {
@@ -8,18 +8,17 @@ if (window.console && window.console.info) {
 $(document).ready(function () {
   window.GOVUKFrontend.initAll()
 
-
-  var baseMapLayer = new ol.layer.Tile({
+  const baseMapLayer = new ol.layer.Tile({
     source: new ol.source.OSM()
-  });
+  })
 
-  var mapboxLayer = new ol.layer.MapboxVector ({
+  const mapboxLayer = new ol.layer.MapboxVector({
     styleUrl: 'mapbox://styles/ant-defra/cknou8uzf5hfw17qzo0076s58',
     accessToken:
-      'pk.eyJ1IjoiYW50LWRlZnJhIiwiYSI6ImNrbmtkaDEyMzA2emQycHFsOW04YjB1eWkifQ.NR7GSXgdwmFKZzLwSti3uA',
-  });
+      'pk.eyJ1IjoiYW50LWRlZnJhIiwiYSI6ImNrbmtkaDEyMzA2emQycHFsOW04YjB1eWkifQ.NR7GSXgdwmFKZzLwSti3uA'
+  })
 
-  var polygonStyle = new ol.style.Style({
+  const polygonStyle = new ol.style.Style({
     fill: new ol.style.Fill({
       color: 'rgba(255, 255, 255, 0.5)'
     }),
@@ -33,9 +32,9 @@ $(document).ready(function () {
       scale: 0.5,
       src: 'public/images/map-draw-cursor-2x.png'
     })
-  });
+  })
 
-  var polygonVertexStyle = new ol.style.Style({
+  const polygonVertexStyle = new ol.style.Style({
     image: new ol.style.Icon({
       opacity: 1,
       size: [32, 32],
@@ -46,124 +45,116 @@ $(document).ready(function () {
     // Return the coordinates of the first ring of the polygon
     geometry: function (feature) {
       if (feature.getGeometry().getType() === 'Polygon') {
-        var coordinates = feature.getGeometry().getCoordinates()[0]
+        const coordinates = feature.getGeometry().getCoordinates()[0]
         return new ol.geom.MultiPoint(coordinates)
       } else {
         return null
       }
     }
-  });
+  })
 
-  var polygonSource = new ol.source.Vector({wrapX: false});
-  var polygonLayer = new ol.layer.Vector({
+  const polygonSource = new ol.source.Vector({ wrapX: false })
+  const polygonLayer = new ol.layer.Vector({
     source: polygonSource,
     style: [polygonStyle, polygonVertexStyle]
-  });
-
-
+  })
 
   // If statement that changes the map layers on the confirm page
   // I need to look at pin location changes next.
-  if(document.getElementById("map").classList.contains("map--confirm")){
+  if (document.getElementById('map').classList.contains('map--confirm')) {
     var map = new ol.Map({
       target: 'map',
-      layers: [ baseMapLayer, polygonLayer ],
+      layers: [baseMapLayer, polygonLayer],
       view: new ol.View({
         center: ol.proj.fromLonLat([-2.571657, 53.381048]),
-        zoom: 15, //Initial Zoom Level
+        zoom: 15, // Initial Zoom Level
         maxZoom: 17
       }),
       interactions: ol.interaction.defaults({
         altShiftDragRotate: false,
         pinchRotate: false
       })
-    });
-  } else if (document.getElementById("map").classList.contains("map--fz1")) {
+    })
+  } else if (document.getElementById('map').classList.contains('map--fz1')) {
     var map = new ol.Map({
       target: 'map',
-      layers: [ mapboxLayer, polygonLayer ],
+      layers: [mapboxLayer, polygonLayer],
       view: new ol.View({
         center: ol.proj.fromLonLat([-2.564057, 53.378333]),
-        zoom: 15 //Initial Zoom Level
+        zoom: 15 // Initial Zoom Level
       }),
       interactions: ol.interaction.defaults({
         altShiftDragRotate: false,
         pinchRotate: false
       })
-    });
-  }
-  else if (document.getElementById("map").classList.contains("map--fz2")) {
+    })
+  } else if (document.getElementById('map').classList.contains('map--fz2')) {
     var map = new ol.Map({
       target: 'map',
-      layers: [ mapboxLayer, polygonLayer ],
+      layers: [mapboxLayer, polygonLayer],
       view: new ol.View({
-        center: ol.proj.fromLonLat([-2.570489, 53.381511]),
-        zoom: 15 //Initial Zoom Level
+        center: ol.proj.fromLonLat([-2.564057, 53.378333]),
+        zoom: 15 // Initial Zoom Level
       }),
       interactions: ol.interaction.defaults({
         altShiftDragRotate: false,
         pinchRotate: false
       })
-    });
-  }
-  else if (document.getElementById("map").classList.contains("map--fz3")) {
+    })
+  } else if (document.getElementById('map').classList.contains('map--fz3')) {
     var map = new ol.Map({
       target: 'map',
-      layers: [ mapboxLayer, polygonLayer ],
+      layers: [mapboxLayer, polygonLayer],
       view: new ol.View({
-        center: ol.proj.fromLonLat([-2.570489, 53.385511]),
-        zoom: 15 //Initial Zoom Level
+        center: ol.proj.fromLonLat([-2.564057, 53.378333]),
+        zoom: 15 // Initial Zoom Level
       }),
       interactions: ol.interaction.defaults({
         altShiftDragRotate: false,
         pinchRotate: false
       })
-    });
-  }
-  else if (document.getElementById("map").classList.contains("map--fzd")) {
+    })
+  } else if (document.getElementById('map').classList.contains('map--fzd')) {
     var map = new ol.Map({
       target: 'map',
-      layers: [ mapboxLayer, polygonLayer ],
+      layers: [mapboxLayer, polygonLayer],
       view: new ol.View({
-        center: ol.proj.fromLonLat([-2.576372, 53.382467]),
-        zoom: 15 //Initial Zoom Level
+        center: ol.proj.fromLonLat([-2.564057, 53.378333]),
+        zoom: 15 // Initial Zoom Level
       }),
       interactions: ol.interaction.defaults({
         altShiftDragRotate: false,
         pinchRotate: false
       })
-    });
-  }
-  else {
-    //Swaps the map to the FLood Zone Layers from Mapbox Studio
+    })
+  } else {
+    // Swaps the map to the FLood Zone Layers from Mapbox Studio
     var map = new ol.Map({
       target: 'map',
-      layers: [ mapboxLayer, polygonLayer ],
+      layers: [mapboxLayer, polygonLayer],
       view: new ol.View({
-        center: ol.proj.fromLonLat([-2.571657, 53.381048]),
-        zoom: 15 //Initial Zoom Level
+        center: ol.proj.fromLonLat([-2.564057, 53.378333]),
+        zoom: 15 // Initial Zoom Level
       }),
       interactions: ol.interaction.defaults({
         altShiftDragRotate: false,
         pinchRotate: false
       })
-    });
+    })
   }
-
-
 
   // Click handler for pointer
   map.on('singleclick', function (e) {
     if (markerLayer.getVisible() === true) {
-      var point = markerSource.getFeatures()[0];
+      const point = markerSource.getFeatures()[0]
       point.getGeometry().setCoordinates([e.coordinate[0], e.coordinate[1]])
     }
   })
 
-  /////// POLYGON STUF ///////
+  /// //// POLYGON STUF ///////
 
   // Modify polygon drawing style
-  var modifyStyle = new ol.style.Style({
+  const modifyStyle = new ol.style.Style({
     fill: new ol.style.Fill({
       color: 'rgba(255, 255, 255, 0.5)'
     }),
@@ -177,15 +168,15 @@ $(document).ready(function () {
       scale: 0.5,
       src: '/public/images/map-draw-cursor-2x.png'
     })
-  });
+  })
 
-  var modify = new ol.interaction.Modify({
+  const modify = new ol.interaction.Modify({
     source: polygonSource,
     style: modifyStyle
-  });
+  })
 
   // Draw polygon drawing style
-  var drawStyle = new ol.style.Style({
+  const drawStyle = new ol.style.Style({
     fill: new ol.style.Fill({
       color: 'rgba(255, 255, 255, 0.5)'
     }),
@@ -199,95 +190,88 @@ $(document).ready(function () {
       scale: 0.5,
       src: '/public/images/map-draw-cursor-2x.png'
     })
-  });
+  })
 
-  var draw = new ol.interaction.Draw({
+  const draw = new ol.interaction.Draw({
     source: polygonSource,
     type: 'Polygon',
     style: drawStyle
-  });
+  })
 
   draw.on('drawend', function (e) {
-    var coordinates = e.feature.getGeometry().getCoordinates()[0]
+    const coordinates = e.feature.getGeometry().getCoordinates()[0]
     if (coordinates.length >= 4) {
       setTimeout(function () {
         map.removeInteraction(draw)
-      }, 500);
+      }, 500)
     }
-  });
+  })
 
-  var snap = new ol.interaction.Snap({
+  const snap = new ol.interaction.Snap({
     source: polygonSource
-  });
+  })
 
-  function addInteractions() {
+  function addInteractions () {
     if (polygonSource.getFeatures().length === 0) {
-      map.addInteraction(draw);
+      map.addInteraction(draw)
     }
-    map.addInteraction(modify);
-    map.addInteraction(snap);
+    map.addInteraction(modify)
+    map.addInteraction(snap)
   }
 
-  function removeInteractions() {
-    map.removeInteraction(draw);
-    map.removeInteraction(modify);
-    map.removeInteraction(snap);
+  function removeInteractions () {
+    map.removeInteraction(draw)
+    map.removeInteraction(modify)
+    map.removeInteraction(snap)
   }
   // Changes to make the polygon available
-  if (document.getElementById("map").classList.contains("map--justboundary")) {
-    addInteractions()
-    document.getElementById("deleteShapeBtn").disabled = false
-    polygonLayer.setVisible(true)
-    markerLayer.setVisible(false)
-  }
+  //  if (document.getElementById("map").classList.contains("map--justboundary")) {
+  addInteractions()
+  document.getElementById('deleteShapeBtn').disabled = false
+  polygonLayer.setVisible(true)
+  markerLayer.setVisible(false)
+  //  }
   // End of changes to make the polygon available
 
-  var radios = document.getElementsByName('marker-or-shape');
-  for (var i = 0, length = radios.length; i < length; i++) {
-    radios[i].addEventListener('click', function(event) {
-
-      var deleteBtn = document.getElementById("deleteShapeBtn");
+  const radios = document.getElementsByName('marker-or-shape')
+  for (let i = 0, length = radios.length; i < length; i++) {
+    radios[i].addEventListener('click', function (event) {
+      const deleteBtn = document.getElementById('deleteShapeBtn')
 
       if (this.value == 'draw-shape') {
-        addInteractions();
-        deleteBtn.disabled = false;
-        polygonLayer.setVisible(true);
-        markerLayer.setVisible(false);
+        addInteractions()
+        deleteBtn.disabled = false
+        polygonLayer.setVisible(true)
+        markerLayer.setVisible(false)
       } else if (this.value == 'delete-shape' &&
         polygonSource.getFeatures().length === 1) {
-          polygonSource.removeFeature(polygonSource.getFeatures()[0]);
+        polygonSource.removeFeature(polygonSource.getFeatures()[0])
       } else if (this.value === 'move-marker') {
-        removeInteractions();
-        deleteBtn.disabled = true;
-        polygonLayer.setVisible(false);
-        markerLayer.setVisible(true);
+        removeInteractions()
+        deleteBtn.disabled = true
+        polygonLayer.setVisible(false)
+        markerLayer.setVisible(true)
       }
-    }, false);
+    }, false)
   }
 
-  var buttons = document.getElementsByName('delete-shape');
+  const buttons = document.getElementsByName('delete-shape')
   if (buttons.length === 1) {
-    buttons[0].addEventListener('click', function(event) {
+    buttons[0].addEventListener('click', function (event) {
       if (polygonSource.getFeatures().length === 1) {
-          polygonSource.removeFeature(polygonSource.getFeatures()[0]);
+        polygonSource.removeFeature(polygonSource.getFeatures()[0])
       }
 
-      map.addInteraction(draw);
-      document.getElementById("draw-shape").focus();
-
-    });
+      map.addInteraction(draw)
+      document.getElementById('draw-shape').focus()
+    })
   }
 
   // EVENT HANDLER FOR CUSTOM DETAILS COMPONENT
-  $('.map-toggle').on('click', function(e) {
-    e.preventDefault();
-    $(this).toggleClass('active');
-  });
+  $('.map-toggle').on('click', function (e) {
+    e.preventDefault()
+    $(this).toggleClass('active')
+  })
 
   // Add a polygon to the map
-
-
-
-
-
 })

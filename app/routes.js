1539175fd342aef65451e8f43057de3d493bc0a8
christina gyles <christinagyles@Emis-MacBook-Pro.local>
2023-07-26 22:03:37 +0100

    AEP and historic flood outlines done, part way through historic. then to update alt tags, tables and version the routes

--- app/routes.js
+++ app/routes.js
@@ -8,4 +8,27 @@ const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
 
+    // Area of P4 for usability testing ==============================================================
+
+    router.get('/P4testingv1/area', function (req, res) {
+        res.render('P4testingv1/area',{
+            "formAction":"/P4testingv1/area-check"
+        })
+      })
+    
+      router.post('/P4testingv1/area', function (req, res) {
+        res.render('/P4testingv1/area',{
+            "formAction":"/P4testingv1/area-check"
+        })
+      })
+      
+      // Route to check if address or area has been selected
+      router.post('/P4testingv1/area-check', function (req, res) {
+      
+        if (req.body['chooseptype']=="tidal") {
+          res.redirect("tidal")
+        } else {
+          res.redirect("options")
+        }
+      })
 

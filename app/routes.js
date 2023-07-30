02699acb9e66b13f6b99b9b5c0c00511b472d132
christina gyles <christinagyles@Emis-MacBook-Pro.local>
2023-07-30 12:09:15 +0100

    redirects to get to each design option page with session data defind content

--- app/routes.js
+++ app/routes.js
@@ -12,9 +12,41 @@ router.get('/cls', function (req, res) {
 	res.render('index')
   })
 
+// set up route variable default status
+router.get('/rflood_O1', function (req, res) {
+	req.session.data = { recorded_flood: 'option1' }
+	res.redirect(`/v1/recorded_flood`)
+})
 
+// set up route variable default status
+router.get('/rflood_O2', function (req, res) {
+	req.session.data = { recorded_flood: 'option2' }
+	res.redirect(`/v1/recorded_flood`)
+})
 
+// set up route variable default status
+router.get('/rflood_O3', function (req, res) {
+	req.session.data = { recorded_flood: 'option3' }
+	res.redirect(`/v1/recorded_flood`)
+})
 
+// set up route variable default status
+router.get('/AEPs_O1', function (req, res) {
+	req.session.data = { aep_option: 'option1' }
+	res.redirect(`/v1/P4AEP`)
+})
+
+// set up route variable default status
+router.get('/AEPs_O2', function (req, res) {
+	req.session.data = { aep_option: 'option2' }
+	res.redirect(`/v1/P4AEP`)
+})
+
+// set up route variable default status
+router.get('/AEPs_O3', function (req, res) {
+	req.session.data = { aep_option: 'option3' }
+	res.redirect(`/v1/P4AEP`)
+})
 // Area of P4 for usability testing ==============================================================
 router.get('/v1/area', function (req, res) {
     res.render('v1/area',{
